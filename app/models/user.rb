f7a926575eb72e7b85c818d3f09eface96a34eb0
Jake Bills <jakebills1@gmail.com>
2019-12-27 09:47:28 -0700

    fixes devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  
 end
